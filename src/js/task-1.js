d0e5b8268aba1d07086c120c563b990dfba2fe9a
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-12 01:43:12 +0300

    Update task-1.js

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,10 +1,9 @@
 const delay = ms => {
   
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
 
         setTimeout(() => {
             resolve(ms);
-            reject('error');
         }, ms)
     })
   };
@@ -13,7 +12,7 @@ const delay = ms => {
   const errorCatch = error => console.log(error);
   
   // Вызовы функции для проверки
-  delay(2000).then(logger).catch(errorCatch); // Resolved after 2000ms
-  delay(1000).then(logger).catch(errorCatch); // Resolved after 1000ms
-  delay(1500).then(logger).catch(errorCatch); // Resolved after 1500ms
+  delay(2000).then(logger): // Resolved after 2000ms
+  delay(1000).then(logger); // Resolved after 1000ms
+  delay(1500).then(logger); // Resolved after 1500ms
 
